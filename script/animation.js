3b36ab00400a9200098738fdb2241eb98bb0f8f6
Miłosz Idzikowski <kontakt@midzikowski.pl>
2021-02-01 10:58:53 +0100

    +animation loop, +one more animation effect

--- script/animation.js
+++ script/animation.js
@@ -49,28 +49,31 @@ const startImgAnimation = () => {
     const tl = gsap.timeline({ defaults: { ease: Back.easeOut.config(2) } });
     
     tl.to(button, { opacity: 0, display: 'none', ease: 'ease'});
-    tl.to(svg1, { display: 'block' });
+    tl.to(svg1, { display: 'block', opacity: 1 });
     [frame, block1, block2, block3].forEach(element => {
         tl.fromTo(element, { y: `+=${baseY}` }, { autoAlpha: 1, y: `-=${baseY}`});
     });
     [text1, text2, text4].forEach(element => {
         tl.fromTo(element, { x: `+=${baseX}` }, { autoAlpha: 1, x: `-=${baseX}`}, '-=.3');
-    })
+    });
     tl.fromTo(guy2, { scale: 0 }, { scale: 1, autoAlpha: 1})
-        .fromTo(subFrame1, { y: `-=${baseY}` }, { autoAlpha: 1, y: `+=${baseY }`, ease: 'elastic', duration: 1})
+        .fromTo(subFrame1, { y: `-=${baseY}` }, { autoAlpha: 1, y: `+=${baseY }`, ease: 'elastic', duration: 1 })
         .to(guy4, { autoAlpha: 1, ease: 'ease', duration: 1})
         .fromTo(guy1, { y: `+=${baseY}` }, { y: `-=${baseY}`, autoAlpha: 1 })
         .fromTo(guy3, { y: `-=${baseY}` }, { y: `+=${baseY}`, autoAlpha: 1 })
-        .fromTo(subFrame2, { y: `-=${baseY}` }, { autoAlpha: 1, y: `+=${baseY }`, ease: 'elastic', duration: 1})
+        .to([guy1, guy3], { x: `-=${baseX}` })
+        .to([guy1, guy3], { x: `+=${baseX}` })
+        .fromTo(subFrame2, { y: `-=${baseY}` }, { autoAlpha: 1, y: `+=${baseY }`, ease: 'elastic', duration: 1 })
         .to(svg1, { opacity: 0, ease: 'ease', duration: .3 })
         .to(svg1, { display: 'none', duration: .1 });
 
-    gsap.set([...boxElements], { transformOrigin: 'left' });
+    gsap.set([...boxElements], { transformOrigin: 'left', backgroundColor: 'transparent' });
+    gsap.set(boxContainer, { skewX: '0 0', scale: 1, y: 0, x: 0 });
 
     const tl2 = gsap.timeline({ defaults: { ease: 'power2.inOut' }});
-    tl2.delay(tl.endTime() - 1);
+    tl2.delay(tl.totalDuration());
     
-    tl2.to(boxContainer, { visibility: 'inherit' });
+    tl2.to(boxContainer, { visibility: 'inherit', display: 'grid' });
     [...boxElements].forEach(element => {
         tl2.fromTo(element, { scaleX: 0 }, { scaleX: 1, backgroundColor: '#f2f2f2', duration: .2 })
     });
@@ -78,9 +81,14 @@ const startImgAnimation = () => {
         .to([...boxElements], { backgroundColor: '#1A1C29'})
         .to([...boxElements], { scaleX: .5, transformOrigin: 'center' })
         .to(boxContainer, { skewX: '-20deg -20deg' })
-        .to(boxContainer, { scale: 0, y: `-=500`, x: `+=1000`, duration: 1.2 });
-    
-        
+        .to(boxContainer, { scale: 0, y: `-=500`, x: `+=1000`, duration: 1.2 })
+        .to(boxContainer, { display: 'none', duration: .1 });
+    tl2.to(document.body, { backgroundColor: '#FFFFFF' });
+
+    tl2.to(button, { opacity: 1, display: 'block' });
+    setTimeout(() => {
+        button.textContent = 'Again?';
+    }, tl.totalDuration() * 1000);
 }
 
 button.addEventListener('mouseenter', buttonHoverAnimation);
