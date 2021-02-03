274ab2b94a2765477c386c15b62be68d10553660
Miłosz Idzikowski <kontakt@midzikowski.pl>
2021-02-03 22:13:10 +0100

    cleanup code

--- script/animation.js
+++ script/animation.js
@@ -19,77 +19,108 @@ const rule = CSSRulePlugin.getRule('button:after');
 
 const tl = gsap.timeline();
 
-tl.to(rule, {duration: .2, zIndex: -10, transform: 'translate(8%, 8%)', filter: 'blur(.4em)', ease: 'easeInOut'});
-tl.reversed(true);      
+tl.to(rule, {
+  duration: 0.2,
+  zIndex: -10,
+  transform: 'translate(8%, 8%)',
+  filter: 'blur(.4em)',
+  ease: 'easeInOut',
+});
+tl.reversed(true);
 
 const buttonHoverAnimation = () => {
-    tl.reversed(!tl.reversed());
-}
+  tl.reversed(!tl.reversed());
+};
 
 const startImgAnimation = () => {
-    const baseY = 300;
-    const baseX = 100;
-    const boxElements = boxContainer.children;
+  const baseY = 300;
+  const baseX = 100;
+  const boxElements = boxContainer.children;
 
-    gsap.set([
-        frame, 
-        block1, 
-        block2, 
-        block3, 
-        text1, 
-        text2, 
-        text4, 
-        subFrame1, 
-        subFrame2, 
-        guy1, 
-        guy2, 
-        guy3, 
-        guy4], { autoAlpha: 0 });
+  gsap.set(
+    [
+      frame,
+      block1,
+      block2,
+      block3,
+      text1,
+      text2,
+      text4,
+      subFrame1,
+      subFrame2,
+      guy1,
+      guy2,
+      guy3,
+      guy4,
+    ],
+    { autoAlpha: 0 },
+  );
 
-    const tl = gsap.timeline({ defaults: { ease: Back.easeOut.config(2) } });
-    
-    tl.to(button, { opacity: 0, display: 'none', ease: 'ease'});
-    tl.to(svg1, { display: 'block', opacity: 1 });
-    [frame, block1, block2, block3].forEach(element => {
-        tl.fromTo(element, { y: `+=${baseY}` }, { autoAlpha: 1, y: `-=${baseY}`});
-    });
-    [text1, text2, text4].forEach(element => {
-        tl.fromTo(element, { x: `+=${baseX}` }, { autoAlpha: 1, x: `-=${baseX}`}, '-=.3');
-    });
-    tl.fromTo(guy2, { scale: 0 }, { scale: 1, autoAlpha: 1})
-        .fromTo(subFrame1, { y: `-=${baseY}` }, { autoAlpha: 1, y: `+=${baseY }`, ease: 'elastic', duration: 1 })
-        .to(guy4, { autoAlpha: 1, ease: 'ease', duration: 1})
-        .fromTo(guy1, { y: `+=${baseY}` }, { y: `-=${baseY}`, autoAlpha: 1 })
-        .fromTo(guy3, { y: `-=${baseY}` }, { y: `+=${baseY}`, autoAlpha: 1 })
-        .to([guy1, guy3], { x: `-=${baseX}` })
-        .to([guy1, guy3], { x: `+=${baseX}` })
-        .fromTo(subFrame2, { y: `-=${baseY}` }, { autoAlpha: 1, y: `+=${baseY }`, ease: 'elastic', duration: 1 })
-        .to(svg1, { opacity: 0, ease: 'ease', duration: .3 })
-        .to(svg1, { display: 'none', duration: .1 });
+  const tl = gsap.timeline({ defaults: { ease: Back.easeOut.config(2) } });
 
-    gsap.set([...boxElements], { transformOrigin: 'left', backgroundColor: 'transparent' });
-    gsap.set(boxContainer, { skewX: '0 0', scale: 1, y: 0, x: 0 });
+  tl.to(button, { opacity: 0, display: 'none', ease: 'ease' });
+  tl.to(svg1, { display: 'block', opacity: 1 });
+  [frame, block1, block2, block3].forEach(element => {
+    tl.fromTo(element, { y: `+=${baseY}` }, { autoAlpha: 1, y: `-=${baseY}` });
+  });
+  [text1, text2, text4].forEach(element => {
+    tl.fromTo(
+      element,
+      { x: `+=${baseX}` },
+      { autoAlpha: 1, x: `-=${baseX}` },
+      '-=.3',
+    );
+  });
+  tl.fromTo(guy2, { scale: 0 }, { scale: 1, autoAlpha: 1 })
+    .fromTo(
+      subFrame1,
+      { y: `-=${baseY}` },
+      { autoAlpha: 1, y: `+=${baseY}`, ease: 'elastic', duration: 1 },
+    )
+    .to(guy4, { autoAlpha: 1, ease: 'ease', duration: 1 })
+    .fromTo(guy1, { y: `+=${baseY}` }, { y: `-=${baseY}`, autoAlpha: 1 })
+    .fromTo(guy3, { y: `-=${baseY}` }, { y: `+=${baseY}`, autoAlpha: 1 })
+    .to([guy1, guy3], { x: `-=${baseX}` })
+    .to([guy1, guy3], { x: `+=${baseX}` })
+    .fromTo(
+      subFrame2,
+      { y: `-=${baseY}` },
+      { autoAlpha: 1, y: `+=${baseY}`, ease: 'elastic', duration: 1 },
+    )
+    .to(svg1, { opacity: 0, ease: 'ease', duration: 0.3 })
+    .to(svg1, { display: 'none', duration: 0.1 });
 
-    const tl2 = gsap.timeline({ defaults: { ease: 'power2.inOut' }});
-    tl2.delay(tl.totalDuration());
-    
-    tl2.to(boxContainer, { visibility: 'inherit', display: 'grid' });
-    [...boxElements].forEach(element => {
-        tl2.fromTo(element, { scaleX: 0 }, { scaleX: 1, backgroundColor: '#f2f2f2', duration: .2 })
-    });
-    tl2.to(document.body, { backgroundColor: '#11121B' })
-        .to([...boxElements], { backgroundColor: '#1A1C29'})
-        .to([...boxElements], { scaleX: .5, transformOrigin: 'center' })
-        .to(boxContainer, { skewX: '-20deg -20deg' })
-        .to(boxContainer, { scale: 0, y: `-=500`, x: `+=1000`, duration: 1.2 })
-        .to(boxContainer, { display: 'none', duration: .1 });
-    tl2.to(document.body, { backgroundColor: '#FFFFFF' });
+  gsap.set([...boxElements], {
+    transformOrigin: 'left',
+    backgroundColor: 'transparent',
+  });
+  gsap.set(boxContainer, { skewX: '0 0', scale: 1, y: 0, x: 0 });
 
-    tl2.to(button, { opacity: 1, display: 'block' });
-    setTimeout(() => {
-        button.textContent = 'Again?';
-    }, tl.totalDuration() * 1000);
-}
+  const tl2 = gsap.timeline({ defaults: { ease: 'power2.inOut' } });
+  tl2.delay(tl.totalDuration());
+
+  tl2.to(boxContainer, { visibility: 'inherit', display: 'grid' });
+  [...boxElements].forEach(element => {
+    tl2.fromTo(
+      element,
+      { scaleX: 0 },
+      { scaleX: 1, backgroundColor: '#f2f2f2', duration: 0.2 },
+    );
+  });
+  tl2
+    .to(document.body, { backgroundColor: '#11121B' })
+    .to([...boxElements], { backgroundColor: '#1A1C29' })
+    .to([...boxElements], { scaleX: 0.5, transformOrigin: 'center' })
+    .to(boxContainer, { skewX: '-20deg -20deg' })
+    .to(boxContainer, { scale: 0, y: `-=500`, x: `+=1000`, duration: 1.2 })
+    .to(boxContainer, { display: 'none', duration: 0.1 });
+  tl2.to(document.body, { backgroundColor: '#FFFFFF' });
+
+  tl2.to(button, { opacity: 1, display: 'block' });
+  setTimeout(() => {
+    button.textContent = 'Again?';
+  }, tl.totalDuration() * 1000);
+};
 
 button.addEventListener('mouseenter', buttonHoverAnimation);
 button.addEventListener('mouseout', buttonHoverAnimation);
